0ad8d0d46d81c776b8e7f5979acd40106cf20bd4
beto <mMo73vhqEBT>
2019-12-03 22:27:01 +0100

    comment

--- betoflowable/src/main/java/com/beto/flowable/service/SendMailService.java
+++ betoflowable/src/main/java/com/beto/flowable/service/SendMailService.java
@@ -3,6 +3,9 @@ package com.beto.flowable.service;
 import org.flowable.engine.delegate.DelegateExecution;
 import org.flowable.engine.delegate.JavaDelegate;
 
+/**
+ * SendMailService
+ */
 public class SendMailService implements JavaDelegate {
     public void execute(DelegateExecution execution) {
         System.out.println("Sending rejection mail to author.");
